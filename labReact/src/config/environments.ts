aac05f72ea1105a249243dc861079c198d0304a4
alina_akimova <linakim107@gmail.com>
2022-12-11 06:11:57 +0600

    feature: add env file

--- labReact/src/config/environments.ts
+++ labReact/src/config/environments.ts
@@ -1,6 +1,7 @@
+const envs = import.meta.env;
 export default {
-  apiKey: '3e350a3a05fd7c13553c6af6cba2d394',
-  baseApiUrl: 'https://gateway.marvel.com/',
-  ts: 1,
-  hash: '117a8e8ce145c5f147de729091e78126'
+  apiKey: envs.VITE_API_KEY,
+  baseApiUrl: envs.VITE_BASE_API_URL,
+  ts: envs.VITE_TS,
+  hash: envs.VITE_HASH
 };
